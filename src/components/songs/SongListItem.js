d8438f1d5526cd9cd40b9d15134dbc9e5b14c0e3
Aaron Resch <aaronmresch@gmail.com>
2021-01-27 15:56:18 -0600

    styling song table

--- src/components/songs/SongListItem.js
+++ src/components/songs/SongListItem.js
@@ -6,13 +6,13 @@ export const SongListItem = ({ song, tuning, user }) => {
     <tr className="song-list-item">
       <td>{song.artist.name}</td>
       <td>{song.title}</td>
-      <td>{song.genre.name}</td>
-      <td>{tuning.instrument.name}</td>
-      <td>{tuning.name}</td>
-      <td><i className="far fa-plus-square"></i></td>
-      <td><a href={song.url} target="_blank" rel="noreferrer"><i className="fas fa-guitar"></i></a></td>
-      <td><a href={song.youtube} target="_blank" rel="noreferrer"><i className="fab fa-youtube"></i></a></td>      
-      <td>{user.username}</td>
+      <td className="text-center">{song.genre.name}</td>
+      <td className="text-center">{tuning.instrument.name}</td>
+      <td className="text-center">{tuning.name}</td>
+      <td className="text-center"><i className="far fa-plus-square fa-2x"></i></td>
+      <td className="text-center"><a href={song.url} target="_blank" rel="noreferrer"><i className="fas fa-guitar fa-2x"></i></a></td>
+      <td className="text-center"><a href={song.youtube} target="_blank" rel="noreferrer"><i className="fab fa-youtube fa-2x"></i></a></td>      
+      <td className="text-center">{user.username}</td>
     </tr>
   );
 };
